c96177bfb71585af7ef0b18457dc6b692a6efebb
Rogerio Bonatti <rogeriobonatti@gmail.com>
2021-10-14 17:06:42 -0700

    remove p3

--- src/fake_vesc_driver_node.py
+++ src/fake_vesc_driver_node.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 # Copyright (c) 2019, The Personal Robotics Lab, The MuSHR Team, The Contributors of MuSHR
 # License: BSD 3-Clause. See LICENSE.md file in root directory.
